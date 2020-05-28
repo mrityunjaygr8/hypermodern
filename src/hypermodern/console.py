e69fcf0ada94b0bbb1288c9a0298092f12a91a3d
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2020-05-28 19:31:15 +0000

    gitignore added

--- src/hypermodern/console.py
+++ src/hypermodern/console.py
@@ -5,18 +5,26 @@ import requests
 
 from . import __version__
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+API_URL = "https://{}.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 @click.command()
 @click.version_option(version=__version__)
-def main():
+@click.option(
+    "--language",
+    default="en",
+    help="The language of Wikipedia to query"
+)
+def main(language):
     """
     The Hypermodern python project
     """
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
+    try:
+        with requests.get(API_URL.format(language)) as response:
+            response.raise_for_status()
+            data = response.json()
+    except requests.exceptions.HTTPError as e:
+        raise click.ClickException(f"An error has occurred:\n{e}")
 
     title = data["title"]
     extract = data["extract"]
